77ca61ee8ca57e1a960d0ed15f4fa3a6cfb6bc9b
Anand Balivada <46479869+Poincare057@users.noreply.github.com>
2020-11-10 12:53:09 +0530

    Add files via upload

--- Diffraction Guru/Monte_Carlo_Fresnel.py
+++ Diffraction Guru/Monte_Carlo_Fresnel.py
@@ -39,11 +39,11 @@ def pattern(xsize = 0.003, ysize = 0.00003, z = 1, l = 0.000006):
             r = [-k*kstep/2 + itx*kstep, -k*kstep/2 + ity*kstep]
             for monti in range(n):
                 f_real += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).real)/n
-                f2_real += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).real)**2 / n
-                f_im += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).imag)/ n
-                f2_im += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).imag)**2 / n
-            phi_real[ity][itx] = V*f_real + ((-1)**random.randint(1, 10))*math.sqrt(abs(0.5*(f2_real - f_real**2)/n))
-            phi_im[ity][itx] = V*f_im + ((-1)**random.randint(1,10))*math.sqrt(abs(0.5*(f2_im - f_im**2)/n))
+                f2_real += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).real)**2 /n       #Not needed 
+                f_im += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).imag)/ n   
+                f2_im += (E(r[0] - montepoints[monti][0], r[1] - montepoints[monti][1], z, l).imag)**2 /n         #Not needed
+            phi_real[ity][itx] = V*f_real 
+            phi_im[ity][itx] = V*f_im 
             phi[ity][itx] = (phi_real[ity][itx]**2 + phi_im[ity][itx]**2)/(l**2)
     return phi
 
